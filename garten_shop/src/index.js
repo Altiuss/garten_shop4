3e69e9a67e51bb4afe52ad01266a8ce3c5fbaa4c
Altiuss <altiuss@inbox.lv>
2023-03-04 18:46:21 +0100

    make changes back

--- garten_shop/src/index.js
+++ garten_shop/src/index.js
@@ -1,16 +1,17 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
 import "./index.css";
 import { Provider } from "react-redux";
 import App from "./App";
-import { HashRouter } from "react-router-dom";
+import { BrowserRouter} from "react-router-dom";
 import { store } from "./store";
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
   <Provider store={store}>
-    <HashRouter>
+    <BrowserRouter>
       <App />
-    </HashRouter>
+    </BrowserRouter>
   </Provider>,
-  document.getElementById("root")
+ 
 );
